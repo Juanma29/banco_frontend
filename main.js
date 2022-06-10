8a3a0cc36018b509f0c4a7bc1e40e9df47d97561
juanma <jmavilam93@gmail.com>
2022-06-10 20:01:05 +0200

    Primer commit

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+Escribe un programa que declare 3 objetos de cada modelo de datos considerado: gestor, cliente, mensaje y transferencia. Los valores de las propiedades de los objetos pueden ser arbitrarios.
+*/
+
+const gestor1 = {
+    id: 1,
+    usuario: 'gestor1',
+    password: 'gestor1',
+    correo: 'gestor1@gmail.com'
+};
+
+const gestor2 = {
+    id: 2,
+    usuario: 'gestor2',
+    password: 'gestor2',
+    correo: 'gestor2@gmail.com'
+};
+
+const gestor3 = {
+    id: 3,
+    usuario: 'gestor3',
+    password: 'gestor3',
+    correo: 'gestor3@gmail.com'
+};
+
+const cliente1 = {
+    id: 1,
+    id_gestor: 1,
+    usuario: 'usuario1',
+    password: 'usuario1',
+    correo: 'usuario1@gmail.com',
+    saldo: 9500
+};
+
+const cliente2 = {
+    id: 2,
+    id_gestor: 2,
+    usuario: 'usuario2',
+    password: 'usuario2',
+    correo: 'usuario2@gmail.com',
+    saldo: 5000
+};
+
+const cliente3 = {
+    id: 3,
+    id_gestor: 3,
+    usuario: 'usuario3',
+    password: 'usuario3',
+    correo: 'usuario3@gmail.com',
+    saldo: 8000
+};
+
