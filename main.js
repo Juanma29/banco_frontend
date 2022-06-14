24146f3e3395a4b034a4da88085f3fe6b92f19b7
juanma <jmavilam93@gmail.com>
2022-06-14 20:26:04 +0200

    copia de seguridad

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 Escribe un programa que declare 3 objetos de cada modelo de datos considerado: gestor, cliente, mensaje y transferencia. Los valores de las propiedades de los objetos pueden ser arbitrarios.
 */
 
+function generarGestores(){
+
+
 const gestor1 = {
     id: 1,
     usuario: 'gestor1',
@@ -15,6 +18,18 @@ const gestor2 = {
     password: 'gestor2',
     correo: 'gestor2@mail.com'
 };
+
+
+const gestores = [gestor1, gestor2,  {
+    id: 3,
+    usuario: 'gestor3',
+    password: 'gestor3',
+    correo: 'gestor3@mail.com'
+}];
+    return gestores;
+}
+
+function generarClientes(){
 
 const cliente1 = {
     id: 1,
@@ -34,16 +49,6 @@ const cliente2 = {
     saldo: 5000
 };
 
-
-const gestores = [gestor1, gestor2,  {
-    id: 3,
-    usuario: 'gestor3',
-    password: 'gestor3',
-    correo: 'gestor3@mail.com'
-}];
-
-console.log(gestores);
-
 const clientes = [cliente1, cliente2];  
     clientes.push({
         id: 3,
@@ -54,10 +59,10 @@ const clientes = [cliente1, cliente2];
         saldo: 1230
     });
     
-// gestores es un array
-// gestor es un objeto con las propiedades: id, usuario, password, correo
-console.log('--- GESTORES ---')
-for (const gestor of gestores) {
+    return clientes;
+}
+
+function mostrarGestor(gestor) {
     console.log(`id: ${gestor.id}`);
     console.log(`usuario: ${gestor.usuario}`);
     console.log(`password: ${gestor.password}`);
@@ -65,19 +70,39 @@ for (const gestor of gestores) {
     console.log('-----');
 }
 
-console.log('--- CLIENTES ---')
-for (const cliente of clientes) {
+function mostrarGestores(gestores) {
+
+    // gestores es un array
+    // gestor es un objeto con las propiedades: id, usuario, password, correo
+    console.log('--- GESTORES ---')
+    for (const gestor of gestores) {
+        mostrarGestor(gestor);
+    }
+}
+
+function mostrarCliente(cliente){
     console.log(`id: ${cliente.id}`);
     console.log(`usuario: ${cliente.usuario}`);
     console.log(`password: ${cliente.password}`);
     console.log(`correo: ${cliente.correo}`);
+    console.log(`saldo: ${cliente.saldo}`);
     console.log('-----');
 }
+
+function mostrarClientes(clientes){
+    console.log('--- CLIENTES ---')
+    for (const cliente of clientes) {
+        mostrarCliente(cliente);
+    }
+}
+
 
 const gestoresJSON = JSON.stringify(gestores);
 console.log(gestoresJSON);
 console.log(gestoresJSON.length);
 
+const clientes = generarClientes();
+const cliente1 = clientes[0];
 const cliente1JSON = JSON.stringify(cliente1);
 console.log(cliente1JSON);
 
